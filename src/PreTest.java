abc9951f1460eac27783d63a8e8bbc74fd5cad6c
Neila <neila.asynur@gmail.com>
2026-03-03 15:35:07 +0700

    pretest

---
+++ src/PreTest.java
@@ -0,0 +1,7 @@
+public class PreTest {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("=============================\nKARTU TANDA MAHASISWA\nNama\t: Neila Faaizah Asynur\nNIM\t:105224028\nProdi\t:Ilmu Komputer\n=============================");
+
+    }
+}
